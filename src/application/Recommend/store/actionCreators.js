0d4b859765033cee713f4edd437fae72df0cde09
ma14700 <244841522@qq.com>
2020-06-15 18:13:54 +0800

    '初步完成首页样式'

---
+++ src/application/Recommend/store/actionCreators.js
@@ -0,0 +1,45 @@
+import * as actionTypes from './constants';
+import { fromJS } from 'immutable';
+import {
+	getBannerRequest,
+	getRecommendListRequest,
+} from '../../../api/request';
+
+export const changeBannerList = (data) => ({
+	type: actionTypes.CHANGE_BANNER,
+	data: fromJS(data),
+});
+export const changeRecommendList = (data) => ({
+	type: actionTypes.CHANGE_RECOMMEND_LIST,
+	data: fromJS(data),
+});
+export const changeEnterLoading = (data) => ({
+	type: actionTypes.CHANGE_ENTER_LOADING,
+	data,
+});
+
+export const getBannerList = () => {
+	return (dispath) => {
+		getBannerRequest()
+			.then((data) => {
+				dispath(changeBannerList(data.banners));
+			})
+			.catch(() => {
+				console.log('error');
+			});
+	};
+};
+
+// 另外在获取推荐歌单后，应把 loading 状态改为 false
+export const getRecommendList = () => {
+	return (dispatch) => {
+		getRecommendListRequest()
+			.then((data) => {
+				dispatch(changeRecommendList(data.result));
+				dispatch(changeEnterLoading(false)); // 改变 loading
+			})
+			.catch(() => {
+				console.log('推荐歌单数据传输错误');
+			});
+	};
+};
